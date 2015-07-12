11e11dc2b1915d805fdfe5c8994d2bbe37668770
chrisruggia <chris@vastgraphics.com>
2015-07-12 15:31:20 -0500

    Update new-wp-loop.php

--- new-wp-loop.php
+++ new-wp-loop.php
@@ -1,29 +1,29 @@
 <?php
-			// set parameters for the new loop
-			$args = array( 
-				'page_id' => 3433,
-				'posts_per_page' => 1
-				);
+// set parameters for the new loop
+$args = array( 
+	'page_id' => 3433,
+	'posts_per_page' => 1
+	);
 
-				$temp = $wp_query; // set the current loop aside in a placeholder variable
-				$wp_query= null; // clear the current loop
-				$wp_query = new WP_Query(); // initialize query
-				$wp_query->query($args); // what to get 
+	$temp = $wp_query; // set the current loop aside in a placeholder variable
+	$wp_query= null; // clear the current loop
+	$wp_query = new WP_Query(); // initialize query
+	$wp_query->query($args); // what to get 
 
-				while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+	while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
 
-				<?php // do stuff with the post now! ?>
-				<div>
-					<h1><?php the_title(); ?></h1>
-					<div>
-					<?php the_content(); ?>
-					</div>
-				</div>
+	<?php // do stuff with the post now! ?>
+	<div>
+		<h1><?php the_title(); ?></h1>
+		<div>
+		<?php the_content(); ?>
+		</div>
+	</div>
 
-				<?php endwhile; // end the loop
+	<?php endwhile; // end the loop
 
-				$wp_query = null; // clear the new loop
-				$wp_query = $temp; // get the current loop back from its hiding place
-				wp_reset_query(); // reset
+	$wp_query = null; // clear the new loop
+	$wp_query = $temp; // get the current loop back from its hiding place
+	wp_reset_query(); // reset
 
 ?>
